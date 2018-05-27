b6946156a0e4f9167e0b62b5b174671f8fa3e226
Your Name <you@example.com>
2018-05-27 20:40:10 +0300

    adapiveimport

---
+++ scripts/adaptiveimport/copy.ps1
@@ -0,0 +1,3 @@
+Copy-Item installed\admin  -destination ..\.. -Force -Recurse
+Copy-Item installed\vendors  -destination ..\.. -Force -Recurse
+Copy-Item installed\system  -destination ..\.. -Force -Recurse
